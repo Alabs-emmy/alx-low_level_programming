a017a766fdd216c35a3254d7fa25b972e62895e1
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-09-11 07:57:44 -0700

    possible combinations of two-digit number ranging from 0-99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits 
+ *        ranging from 0-99 separated by a comma followed by space
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int num1, num2;
+num1 = 0;
+while (num1 < 99)
+{
+num2 = num1 + 1;
+while (num2 < 100)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 != 98 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+num2++;
+}
+num1++;
+}
+putchar('\n');
+return (0);
+}
