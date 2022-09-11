dec80d290dc7e563d84e54eca73091b2321b3ca8
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-09-11 07:59:44 -0700

    removed traiing line from main func description

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two digits 
+ * main - prints all possible different combinations of two digits
  *        ranging from 0-99 separated by a comma followed by space
  *
  * Return: Always 0.
