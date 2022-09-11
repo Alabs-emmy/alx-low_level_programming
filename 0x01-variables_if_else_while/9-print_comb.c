cd28f366025346695fa5adfa164662c36f0f2b60
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-09-11 03:34:43 -0700

    single digit separated by comma and space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - print all single digits in base 10
- *        with a comma and tab between them
+ * main - print single digit number separated
+ *        by comman and space
  *
  * Return: Always 0.
  */
 int main(void)
 {
-int num = 0;
+int num;
+num = 0;
 while (num < 10)
 {
 putchar((num % 10) + '0');
+if (num != 9)
+{
 putchar(',');
 putchar(' ');
+}
 num++;
 }
 putchar('\n');
