15a9b2a8ec0c174e2c00e00233da4d2993174b3f
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-09-11 07:38:21 -0700

    printing all possible three digits combination separated by comma and space

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *        separated by a comma followed by space
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int num1, num2, num3;
+num1 = 0;
+while (num1 < 8)
+{
+num2 = num1 + 1;
+while (num2 < 9)
+{
+num3 = num2 + 1;
+while (num3 < 10)
+{
+putchar((num1 % 10) + '0');
+putchar((num2 % 10) + '0');
+putchar((num3 % 10) + '0');
+if (num1 != 7 || num2 != 8 || num3 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+num3++;
+}
+num2++;
+}
+num1++;
+}
+putchar('\n');
+return (0);
+}
