1a4b5cc4b1fd4321f29e6988bf5d336dd13e10b7
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-09-10 22:35:34 -0700

    all digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit of base 10
+ *        starting from 0
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d",num);
+num++;
+}
+printf("\n");
+return (0);
+}
