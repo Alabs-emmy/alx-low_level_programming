3952c95e7338425dca2b941d2acea402e1855b49
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-04 16:10:44 -0700

    function allocates space for a 2-dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * **aloc_grid - function returns a pointer to a 2 dimensional
+ *		arrays of integer
+ * @width: numbr of columns
+ * @height: number of rows
+ *
+ * Return: Returns a pointer to a 2-dimensional array
+ */
+int **alloc_grid(int width, int height);
+{
+	int j, i, **ptr;
+
+	ptr = malloc(sizeof(*ptr) * height);
+
+	if (ptr == 0 || width == 0 || height == 0)
+		return (NULL);
+
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			ptr[i] = malloc(sizeof(**ptr) * width);
+
+			if (ptr[i] == NULL)
+			{
+				/*Free everything if malloc fails */
+				while (i--)
+					free(ptr[i]);
+				free(ptr);
+				return (NULL);
+			}
+
+			for (j = 0; j < width; j++)
+				ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
+}
