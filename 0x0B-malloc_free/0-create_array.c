5f5acf0a060e7ef46c8b5fe7fa17bf681864abf6
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-04 14:06:50 -0700

    function type corrected

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
  *
  * Return: Returns a pointer to array or NULL if it fails
  */
-cahr *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c);
 {
 	char *ptr;
 
