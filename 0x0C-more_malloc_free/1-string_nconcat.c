2803a55f607a8b908088a97b7ff3d2f91e103cdd
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-06 15:27:22 -0700

    fixed the n bytes to copy

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		if (i < size_1)
 			*(new_string + i) = *(s1 + i);
 		else
-			*(new_string + i) = *(s2 + (size_1 - i));
+			*(new_string + i) = *(s2 + (i - size_1));
 	}
 	*(new_string + i) = '\0';
 	return (new_string);
