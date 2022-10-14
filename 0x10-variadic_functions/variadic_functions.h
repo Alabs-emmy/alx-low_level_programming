590b81532bd49f4ad8ad3e3df97ba7484c922a57
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-13 18:52:16 -0700

    added comment to header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef _VARIADIC_FUNCION_H
 #define _VARIADIC_FUNCTION_H
 
+/**
+ * file: variadic.h
+ * desc: Header file for the functions used in the
+ * 		0x10-variadic_function directory
+ */
+
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
