2613ed1bf37f9811f2439f9190fe2b444d7c2981
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-14 21:44:29 -0700

    prints data of different types

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -15,16 +15,20 @@
  * @print: A function pointer to a function that prints
  *         a data type corresponding to symbol.
  */
-typedef struct printer
+typedef struct data_print
 {
-	char *symbol;
-	void (*print)(va_list arg);
+	char *type;
+	void (*print_data)(va_list arg);
 
-} printer_t;
+} data_print_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
 
 #endif /* _VARIADIC_FUNCTION_H */
