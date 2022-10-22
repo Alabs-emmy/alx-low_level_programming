97dfefa2f15b59c321e8d67a10f6fe47a3c9d71e
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-22 13:28:04 -0700

    adds new node to the begining

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node - function adds a new node to the begining of a list
+ * @head: pointer to the pointet that points to the address of a node
+ * @str: pointer to the string
+ *
+ * Return: Returns the address of the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	char *str1;
+	list_t *ptr;
+
+	if (head == NULL)
+		return (NULL);
+
+	ptr = malloc(sizeof(list_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	str1 = strdup(str);
+
+	for (i = 0; str1[i] != '\0'; i++)
+		;
+	ptr->str = str1;
+	ptr->len = i;
+	ptr->next = NULL;
+
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
