373c954fbde8ce0473a28d655b155921f37719b4
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-22 12:17:37 -0700

    func description added

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * lisy_len - function returns the number sof element in a linked list
+ * list_len - function returns the number sof element in a linked list
  * @h: head pointer to the first node
  *
  * Return: Returns the number of elements in a linked list
