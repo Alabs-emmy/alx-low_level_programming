b57069d9b3d120a82bf2f2221fad69bc988e69bc
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-22 20:40:09 -0700

    free struct

---
+++ 0x12-singly_linked_lists/4-free-list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a struture
+ * @head: pointer to head node
+ *
+ * Return: Returns nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp
+	}
+
+}
