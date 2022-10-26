3114fc7e8b11b5c92dbc00e0547f7216cebd6ff7
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-10-25 18:28:20 -0700

    frees listint_n

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t
+ * @head: pointer that points to the address of the head pointer
+ *
+ * Description: sets head to NULL.
+ * Return: Returns nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
+}
