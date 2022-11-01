0d6e419505dbb51e022c6b0efd6e9fe75224806f
Alabi Emmanuel Oluwatimileyin <eoalabi@student.oauife.edu.ng>
2022-11-01 11:01:29 -0700

    create and write into a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * create_file - function creates a file, if file already exit
+ *		it trauncates it.
+ * @filename: pointer to the file that neds to be created.
+ *
+ * @text_content: pointer to the null terminated string to be
+ *		wriiten into the created file.
+ *
+ * Return: if successful 1, else -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+	else
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
